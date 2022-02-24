93e057bb3e58edbeed19f957c374f19ccd9536ee
satya <satyaprakash.rath@clarivate.com>
2022-02-24 20:35:41 +0530

    adding data model interface and two-way data binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'ps-user-settings-form',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+   userSettings : UserSettings = {
+     name : 'Milton',
+     emailOffers : true,
+     interfaceStyle : 'dark',
+     notes : ' Here are notes',
+     subscriptionStyle : 'Annual'
+  }
 
   constructor() { }
 
